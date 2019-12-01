114939a335e1ae0c65b2e58bced80b876c59c835
server12-max <57803987+server12-max@users.noreply.github.com>
2019-12-01 20:51:46 +0530

    k

--- main-es2015.js
+++ main-es2015.js
@@ -32,7 +32,7 @@ webpackEmptyAsyncContext.id = "./$$_lazy_route_resource lazy recursive";
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = ("<!-- <div class=\"container\"> -->\n  <!-- <app-employees></app-employees> -->\n  <app-employeeform></app-employeeform>\n  <br>\n  <div>\n        <app-employee-list></app-employee-list>\n</div>\n<!-- </div> -->");
+/* harmony default export */ __webpack_exports__["default"] = ("<!-- <div class=\"container\"> -->\n  <!-- <app-employees></app-employees> -->\n  <app-employeeform></app-employeeform>\n  <br>\n  <!-- <div>\n        <app-employee-list></app-employee-list>\n</div> -->\n<!-- </div> -->");
 
 /***/ }),
 
